caf9472e09d8a3fae99692ba0d5073502f6f5b03
Sayanaann <alexsayana@gmail.com>
2022-12-06 20:34:08 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddcourseComponent } from './addcourse/addcourse.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const myRoute:Routes=[
+{
+  path: "",
+  component:AddcourseComponent
+}
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddcourseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
